36b643ded8e60de52d7a2bb905ed61f89c061adc
eylon frisch <you@example.com>
2022-08-02 11:34:35 +0300

    React starter to GitHub Pages

---
+++ src/pages/ImageGallery.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function ImageGallery() {
+  return <div className="image-container page">images</div>;
+}
+
+export default ImageGallery;
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import ImageGallery from "./pages/ImageGallery.jsx";
+
+const routes = [
+  // {
+  //   path:'/:imageId',
+  //   component: ImageDetails,
+  // },
+  {
+    path: "/",
+    component: ImageGallery,
+  },
+];
+
+export default routes;
